9edf3e7086dad2a52331af8a673afee361ecd4c6
Marcel Subirana <msubiranac@gmail.com>
2020-12-29 11:44:50 +0100

    feat(menuitem): create Hats route with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <BrowserRouter>
+      <Route exact path='/' component={Homepage} />
+    </BrowserRouter>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const menuItem = ({title, size, imageUrl}) => {
+const menuItem = ({ title, size, imageUrl, linkUrl, history, match }) => {
   const menuItemImage = {
     backgroundImage: `url(${imageUrl})`,
   };
   // ? bg-img div created as sibling of content, not wrapper, bc when scaling on hover we don't want the content to grow
   return (
-    <div className={`menu-item ${size}`}>
-    <div style={menuItemImage} className="background-image"></div>
+    <div className={`menu-item ${size}`} onClick={()=> history.push(`${match.url}${linkUrl}`)} >
+      <div style={menuItemImage} className="background-image"></div>
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
@@ -17,4 +18,4 @@ const menuItem = ({title, size, imageUrl}) => {
   );
 };
 
-export default menuItem;
+export default withRouter(menuItem);
