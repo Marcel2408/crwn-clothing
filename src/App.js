f1af0af9a8317769ffb4e0367e8b721022f56dea
Marcel Subirana <msubiranac@gmail.com>
2021-01-06 12:58:33 +0100

    feat(header): add header with logo and links to shop and contact pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Header from "./components/header/header.component";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shopPage.component";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Homepage} />
-      <Route exact path='/shop' component={ShopPage} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </>
   );
 }
 
 export default App;
 /*
-style collection-preview
-create collection-item
-style collection-item (except the 'add to cart' btn)
-style padding on body in app.css
+create header
+create links to home (logo) & shop & contact
+style header
 todo commit all changes
 */
